97934af14e99d727b4098b602f9cc93a79d372fc
winkey83 <winkey83@gmail.com>
2022-10-29 13:08:55 +0900

    state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,40 @@ class App extends React.Component {
     count: 0,
   };
 
+  constructor(props) {
+    super(props);
+    console.log('AA: constructor');
+  }
+
+  componentDidMount() {
+    console.log('AA: componentDidMount');
+  }
+
+  componentDidUpdate() {
+    console.log('AA: componentDidUpdate');
+  }
+
+  add = () => {
+    this.setState(current=>({
+      count: current.count+1
+    }));
+  };
+
+  subtract = () => {
+    this.setState(current=>({
+      count: current.count-1
+    }));
+  };
+
   render() {
-    return <h1>BattleField</h1>
+    console.log('AA: render');
+    return (
+      <div>
+        <h1>BattleField:{this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.subtract}>Subtract</button>
+      </div>
+    );
   }
 }
 
